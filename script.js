c2a4a7a242634dae132f666a91b4054d016e128b
artneto <arthur_neto@icloud.com>
2023-06-26 15:12:43 +0000

    add save button for templates

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ let spacingButtons = document.querySelectorAll(".spacing");
 let formatButtons = document.querySelectorAll(".format");
 let scriptButtons = document.querySelectorAll(".script");
 let textTemplate = document.getElementById("textTemplate");
+let template1Button = document.getElementById("template1Button");
+let customTemplateInput = document.getElementById("customTemplateInput");
+let saveCustomTemplateButton = document.getElementById("saveCustomTemplate");
 
 // List of fontlist
 let fontList = [
@@ -49,81 +52,28 @@ const initializer = () => {
   // Default size
   fontSizeRef.value = 3;
 
-  // Templates
-  textTemplate.innerHTML += "<option value=''>Select a Template</option>";
-  textTemplate.innerHTML += "<option value='<strong>Template 1</strong>'>Template 1</option>";
-  textTemplate.innerHTML += "<option value='<em>Template 2</em>'>Template 2</option>";
-  textTemplate.innerHTML += "<option value='<u>Template 3</u>'>Template 3</option>";
-
+  // Add event listener for template selection
   textTemplate.addEventListener("change", () => {
     let selectedTemplate = textTemplate.value;
-    modifyText("insertHTML", false, selectedTemplate);
+    if (selectedTemplate === "template1") {
+      modifyText("insertHTML", false, "<strong>Conteúdo do Template 1</strong>");
+    } else {
+      modifyText("insertHTML", false, selectedTemplate);
+    }
+  });
+
+  // Add event listener for custom template saving
+  saveCustomTemplateButton.addEventListener("click", () => {
+    let customTemplate = customTemplateInput.value;
+    if (customTemplate.trim() !== "") {
+      let option = document.createElement("option");
+      option.value = customTemplate;
+      option.innerHTML = customTemplate;
+      textTemplate.appendChild(option);
+      customTemplateInput.value = "";
+    }
   });
 };
 
-// Main logic
-const modifyText = (command, defaultUi, value) => {
-  // execCommand executes command on selected text
-  document.execCommand(command, defaultUi, value);
-};
-
-// For basic operations that don't need value parameter
-optionsButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    modifyText(button.id, false, null);
-  });
-});
-
-// Options that require value parameter (e.g colors, fonts)
-advancedOptionButton.forEach((button) => {
-  button.addEventListener("change", () => {
-    modifyText(button.id, false, button.value);
-  });
-});
-
-// Link
-linkButton.addEventListener("click", () => {
-  let userLink = prompt("Enter a URL");
-  // If link has http then pass directly else add https
-  if (/http/i.test(userLink)) {
-    modifyText(linkButton.id, false, userLink);
-  } else {
-    userLink = "http://" + userLink;
-    modifyText(linkButton.id, false, userLink);
-  }
-});
-
-// Highlight clicked button
-const highlighter = (className, needsRemoval) => {
-  className.forEach((button) => {
-    button.addEventListener("click", () => {
-      // needsRemoval = true means only one button should be highlighted and others would be normal
-      if (needsRemoval) {
-        let alreadyActive = false;
-
-        // If currently clicked button is already active
-        if (button.classList.contains("active")) {
-          alreadyActive = true;
-        }
-
-        // Remove highlight from other buttons
-        highlighterRemover(className);
-        if (!alreadyActive) {
-          // Highlight clicked button
-          button.classList.add("active");
-        }
-      } else {
-        // If other buttons can be highlighted
-        button.classList.toggle("active");
-      }
-    });
-  });
-};
-
-const highlighterRemover = (className) => {
-  className.forEach((button) => {
-    button.classList.remove("active");
-  });
-};
-
-window.onload = initializer;
+// Initialize the editor
+initializer();
